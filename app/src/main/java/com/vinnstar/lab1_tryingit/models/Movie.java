83d888ca90f6e5c688de1c7d6b5125fe140b3c09
Laurent_Vinn <laurent.mundell@gmail.com>
2019-10-25 15:50:59 -0400

    Ver 2

--- app/src/main/java/com/vinnstar/lab1_tryingit/models/Movie.java
+++ app/src/main/java/com/vinnstar/lab1_tryingit/models/Movie.java
@@ -3,23 +3,31 @@ package com.vinnstar.lab1_tryingit.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
-
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    Double rating;
+
+    //Parcel Library requirement these are c++ guys. Defualt constructor needed.
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         this.backdropPath = jsonObject.getString("backdrop_path");
         this.posterPath = jsonObject.getString("poster_path");
         this.title = jsonObject.getString("title");
         this.overview = jsonObject.getString("overview");
+        this.rating = jsonObject.getDouble("vote_average");
+        this.movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -44,5 +52,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
